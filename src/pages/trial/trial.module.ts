3d1ec98f29ff7e2b28e40a300710df38ab526d8e
Samuel Jackson <samueljackson@Samuels-MBP.home>
2017-10-29 12:37:14 +0000

    test

---
+++ src/pages/trial/trial.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TrialPage } from './trial';
+
+@NgModule({
+  declarations: [
+    TrialPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TrialPage),
+  ],
+})
+export class TrialPageModule {}
